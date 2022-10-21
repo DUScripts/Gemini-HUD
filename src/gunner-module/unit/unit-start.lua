60005d98175fe0d2d9f5d26b230eb78a07d066af
Crusader93 <104208203+Crusader93@users.noreply.github.com>
2022-10-21 15:24:30 +0300

    Update unit-start.lua

--- src/gunner-module/unit/unit-start.lua
+++ src/gunner-module/unit/unit-start.lua
@@ -60,6 +60,8 @@ end
 friendly_IDs = {} -- put IDs here 34141,231231,31231 etc
 
 --vars
+radarIDs = ''
+idN = 0
 screenHeight = system.getScreenHeight()
 screenWidth = system.getScreenWidth()
 lastHitTime = {}
